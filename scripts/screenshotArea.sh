1365fd305e600db0145fa70523bf9acf5bea4971
n5mn <n5mn@users.noreply.github.com>
2024-02-22 15:13:07 -0300

    Fix screenshot script

--- scripts/screenshotArea.sh
+++ scripts/screenshotArea.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 Screenshot="Screenshot_$(date +%Y-%m-%d-%H-%M-%S).png"
-grim -g "$(slurp)" "$HOME/Pictures/Screenshots/$Screenshot" && wl-copy < "$HOME/Pictures/Screenshots/$"
+grim -g "$(slurp)" "$HOME/Pictures/Screenshots/$Screenshot" && wl-copy < "$HOME/Pictures/Screenshots/$Screenshot"
