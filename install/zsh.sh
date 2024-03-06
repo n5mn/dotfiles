92e299459cd724e4eb81bd7b4b69a14eff3ef6e5
n5mn <n5mn@users.noreply.github.com>
2024-03-06 09:58:17 -0300

    fix

--- install/zsh.sh
+++ install/zsh.sh
@@ -5,6 +5,6 @@ InstallZsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
     echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-    ln -sf $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
+    ln -sf $HOME/dotfiles/home/.p10k.zsh $HOME/.p10k.zsh
     source $HOME/.zshrc
 }
