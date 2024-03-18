a3a5a7d25d573177dd0339a6e5d573e6ec7c52ec
n5mn <n5mn@users.noreply.github.com>
2024-03-18 16:11:19 -0300

    Add symlink for cava

--- scripts/update_wallpaper.sh
+++ scripts/update_wallpaper.sh
@@ -12,18 +12,32 @@ cp $wallpaper ~/.cache/current_wallpaper.jpg
 # get wallpaper iamge name
 newwall=$(echo $wallpaper | sed "s|$HOME/dotfiles/wallpapers/||g")
 
+transitions=(
+    "simple"
+    "left"
+    "top"
+    "grow"
+    "center"
+    "any"
+    "outer"
+)
+
+random=$[$RANDOM % ${#transitions[@]}]
+
 # Set the new wallpaper
 swww img $wallpaper \
     --transition-bezier .43,1.19,1,.4 \
     --transition-fps=60 \
-    --transition-type="random" \
-    --transition-duration=0.7 \
+    --transition-type="${transitions[$random]}" \
+    --transition-duration=1.0 \
     --transition-pos "$( hyprctl cursorpos )"
 
 ~/dotfiles/waybar/launch.sh
 
 # Load pywal color scheme for notifications and restart it 
 ln -sf ~/.cache/wal/dunstrc ~/dotfiles/dunst/dunstrc
-killall dunst;notify-send Wallpaper Updated!
 
+ln -sf ~/.cache/wal/cava.conf ~/.config/cava/config
+
+killall dunst;notify-send Wallpaper Updated!
 echo "DONE!"
