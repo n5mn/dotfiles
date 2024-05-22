80945340a72de025a164295241cf9017cf03cd77
n5mn <n5mn@users.noreply.github.com>
2024-05-22 10:12:51 -0400

    Rename functions.zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,7 +4,8 @@ function add_plugin() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2- )
     if [ -d "$ZSH_DOTFILES/plugins/$PLUGIN_NAME" ]; then
         source "$ZSH_DOTFILES/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
-#        source "$ZSH_DOTFILES/plugins/$PLUGIN_NAME.plugin"
+#        source "$ZSH_DOTFILES/plugins/$PLUGIN_NAME.plugin.zsh"
+#        source "ZSH_DOTFILES/plugins/$PLUGIN_NAME.plugin"
     else
         git clone "https://github.com/$1" "$ZSH_DOTFILES/plugins/$PLUGIN_NAME"
 
