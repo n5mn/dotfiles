76dcfede84affbc7b83eccda5905d080527b3613
n5mn <n5mn@users.noreply.github.com>
2024-07-21 22:05:42 -0400

    Cleanup scripts

--- install/installer.sh
+++ install/installer.sh
@@ -1,86 +1,74 @@
 #!/bin/sh
 # Check if a package is installed using pacman
 is_installed_pacman() {
-    package="$1"
-    check="`pacman -Q "$package"`"
-    #[ -n "${check}" ] && echo 0 || echo 1
-    if [ -n "${check}" ]; then
-        return 0
-    else
-        return 1 
-    fi
+	package="$1"
+	check="`pacman -Q "$package"`"
+	#[ -n "${check}" ] && echo 0 || echo 1
+	if [ -n "${check}" ]; then
+		return 0
+	else
+		return 1 
+	fi
 }
 
 # Install packages using pacman if not installed
 install_packages_pacman() {
-    for pkg in "$@"; do
-        if is_installed_pacman "$pkg"; then
-            echo "$pkg is already installed."
-            continue
-        fi
-        sudo pacman --noconfirm -S $pkg
-    done
+	for pkg in "$@"; do
+		if is_installed_pacman "$pkg"; then
+			echo "$pkg is already installed."
+			continue
+		fi
+			sudo pacman --noconfirm -S $pkg
+	done
 }
 
 # Check if a package is installed using yay
 is_installed_aur() {
-    package="$1"
-    check="`yay -Q "$package"`"
-    #[ -n "${check}" ] && echo 0 || echo 1
-    if [ -n "${check}" ]; then
-        return 0
-    else
-        return 1 
-    fi
+	package="$1"
+	check="`yay -Q "$package"`"
+	#[ -n "${check}" ] && echo 0 || echo 1
+	if [ -n "${check}" ]; then
+		return 0
+	else
+		return 1 
+	fi
 }
 
 # Install packages using yay if not installed
 install_packages_aur() {
-    for pkg; do
-        if is_installed_aur "$pkg"; then # TODO: fix this syntax make it compatible with POSIX-compliant shell
-            echo "$pkg is already installed."
-            continue
-        fi
-        yay --noconfirm -S $pkg
-    done
-}
-
-nvidia_drivers() {
-    echo "Install nvidia drivers? [y/N] "
-    read yn
-    case $yn in
-        [yY])
-            sudo pacman -S --needed --noconfirm nvidia nvidia-utils
-            ;;
-        *)
-            echo "Skipping..."
-            ;;
-    esac
+	for pkg; do
+		if is_installed_aur "$pkg"; then # TODO: fix this syntax make it compatible with POSIX-compliant shell
+			echo "$pkg is already installed."
+			continue
+		fi
+		yay --noconfirm -S $pkg
+	done
 }
 
 symlink() {
-    symlink_name="$1"   # This is just the name for logging purposes
-    link_source="$2"
-    link_target="$3"
+	symlink_name="$1"   # This is just the name for logging purposes
+	link_source="$2"
+	link_target="$3"
 
-    # Backing up the old files 
-    backup_directory="$HOME/.config/.backup"
-    if [ ! -d $backup_directory ]; then
-        mkdir -p $backup_directory
-    fi
+	# Backing up the old files 
+	backup_directory="$HOME/.config/.backup"
 
-    if [ -e "$link_target" ] && [ ! -L "$link_target"]; then
-        cp -r "$link_target" "$backup_directory/"
-        echo "Backing up old configuration files!"
-    fi
+	if [ ! -d $backup_directory ]; then
+			mkdir -p $backup_directory
+	fi
 
-    # Remove existing symlink, directory, or file at the target location
-    echo "Removing existing configurations files..."
-    [ -L "$link_target" ] && rm "$link_target"
-    [ -d "$link_target" ] && rm -rf "$link_target"
-    [ -f "$link_target" ] && rm "$link_target"
-    
-    # Create the new symlink
-    ln -s "$link_source" "$link_target"
-    echo "Symlink $link_source -> $link_target created (Named: $symlink_name)."
+	if [ -e "$link_target" ] && [ ! -L "$link_target"]; then
+			cp -r "$link_target" "$backup_directory/"
+			echo "Backing up old configuration files!"
+	fi
+
+	# Remove existing symlink, directory, or file at the target location
+	echo "Removing existing configurations files..."
+	[ -L "$link_target" ] && rm "$link_target"
+	[ -d "$link_target" ] && rm -rf "$link_target"
+	[ -f "$link_target" ] && rm "$link_target"
+	
+	# Create the new symlink
+	ln -sf "$link_source" "$link_target"
+	echo "Symlink $link_source -> $link_target created (Named: $symlink_name)." > $HOME/dotfiles/symlinks.log
 }
--- install/packages.sh
+++ install/packages.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 # Pacman
-packages_pacman="pacman-contrib alacritty starship vim rofi dunst lxappearance-gtk3 breeze breeze-gtk tumbler cliphist ttf-font-awesome ttf-fira-sans ttf-fira-code ttf-firacode-nerd python-pip pavucontrol xdg-desktop-portal-gtk xdg-desktop-portal-wlr networkmanager network-manager-applet networkmanager-openvpn ufw blueman bluez bluez-utils hyprland waybar jq polkit cpio"
-optional_packages_pacman="firefox obs-studio discord spotify-launcher fastfetch gimp htop steam"
+packages_pacman="pacman-contrib alacritty starship vim rofi dunst lxappearance-gtk3 breeze breeze-gtk tumbler cliphist ttf-font-awesome ttf-fira-sans ttf-fira-code ttf-firacode-nerd python-pip pavucontrol xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-desktop-portal-hyprland networkmanager network-manager-applet networkmanager-openvpn ufw hyprland waybar jq polkit cpio inter-font"
+
+bluetooth="blueman bluez bluez-utils"
+
+nvidia_drivers="nvidia nvidia-utils"
+
+optional_packages_pacman="firefox obs-studio spotify-launcher fastfetch gimp htop steam"
+
+# for the gtk theme I use Space-Transparent. git clone --branch=transparent-sidebar https://github.com/EliverLara/Space Space-Transparent
 
 # AUR
-packages_aur="bibata-cursor-theme kora-icon-theme swww wlogout"
-optional_packages_aur="termius brave-bin qownnotes pfetch vesktop"
+packages_aur="bibata-cursor-theme swww wlogout tela-icon-theme"
+optional_packages_aur="termius brave-bin qownnotes vesktop"
