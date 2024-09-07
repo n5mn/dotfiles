3c736a7ac52166e6ee62ed32dc37492fc13c453d
n5mn <n5mn@users.noreply.github.com>
2024-09-07 18:00:19 -0400

    Add conditional check for the notification

--- scripts/screenshot_area.sh
+++ scripts/screenshot_area.sh
@@ -2,4 +2,7 @@
 screenshot="Screenshot_$(date +%Y-%m-%d-%H-%M-%S).png"
 screenshot_path="$HOME/Pictures/Screenshots/$screenshot"
 grim -g "$(slurp)" "$screenshot_path" && wl-copy < "$screenshot_path"
-dunstify -i $screenshot_path "Screenshot saved!" 
+
+if [ -f "$screenshot_path" ]; then
+	dunstify -i $screenshot_path "Screenshot saved!" 
+fi
