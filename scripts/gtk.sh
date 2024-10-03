0e8060feea52a3f2fb740ee3f18ec4255af15629
n5mn <n5mn@users.noreply.github.com>
2024-10-03 15:51:43 -0300

    Change theme

--- scripts/gtk.sh
+++ scripts/gtk.sh
@@ -3,7 +3,7 @@
 # Set GTK Themes, Icons, Cursor and Fonts
 gnome_schema="org.gnome.desktop.interface"
 SCHEMA="gsettings set $gnome_schema"
-THEME="Space-Transparent"
+THEME="Arc-Darker"
 ICONS="Tela-grey"
 FONT="Inter"
 CURSOR="Bibata-Original-Classic"
