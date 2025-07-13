a4ad89d3f4a2f827ad31760a832888d4b1376731
n5mn <n5mn@users.noreply.github.com>
2025-07-13 18:20:13 -0400

    cleanup unused aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -11,26 +11,16 @@ alias la='ls -lah'
 alias l='ls -lh'
 alias dc='docker-compose'
 alias untar="tar -cvzf"
-alias notes='cd ~/Notes/ && nvim .'
-alias dot='cd ~/dotfiles'
-alias own='sudo chown $USER:$USER'
-alias ownR='sudo chown -R $USER:$USER'
 alias root='sudo -E zsh'
 alias ivm='nvim'
 alias vmi='nvim'
 alias vim='nvim'
 alias vi='/usr/bin/vim'
 alias :q='exit'
-alias SS='sudo systemctl'
-alias neofetch='fastfetch'
 alias kubectl='minikube kubectl --' 
 alias tf='terraform'
 alias current_date='date +"%Y-%m-%d_%H-%M"'
 # }}}
 
-if [ -f ~/r2 ]; then
-	source ~/r2
-	alias r2='aws s3 --profile r2 --endpoint-url $R2_ENDPOINT'
-fi
 
 alias g='lazygit'
