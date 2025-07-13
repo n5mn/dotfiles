df0b47816c99bb328d5a9dff44750296fbae5ca4
n5mn <n5mn@users.noreply.github.com>
2025-07-13 19:47:44 -0400

    add snippets

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -14,7 +14,7 @@ return {
 		local cmp = require 'cmp'
 		local luasnip = require 'luasnip'
 		require('luasnip.loaders.from_vscode').lazy_load()
-		require('luasnip.loaders.from_lua').load({ paths = { ".snippets/" } })
+		require('luasnip.loaders.from_lua').lazy_load({ paths = { vim.fn.stdpath("config") .. "/lua/snippets" } })
 		luasnip.config.setup {}
 
 		cmp.setup {
