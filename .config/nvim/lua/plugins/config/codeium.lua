2feb22b2e5f767e7ef8523fec59aa2a50cc2369f
n5mn <n5mn@users.noreply.github.com>
2025-07-13 23:42:27 -0400

    Separate keymaps

--- .config/nvim/lua/plugins/config/codeium.lua
+++ .config/nvim/lua/plugins/config/codeium.lua
@@ -1,11 +1,6 @@
 return {
-	-- AI assistant tool like copilot... but free
 	"Exafunction/codeium.vim",
 	config = function ()
-		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-		vim.keymap.set('i', '<C-.>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-		vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-		vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+		require("plugins.keymaps.codeium")
 	end
 }
