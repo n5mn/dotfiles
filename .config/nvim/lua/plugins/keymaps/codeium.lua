b1b0be179c26f3e43e88f7788c7c7e94ac466fe1
n5mn <n5mn@users.noreply.github.com>
2025-07-14 12:57:22 -0400

    revert keymaps

--- .config/nvim/lua/plugins/keymaps/codeium.lua
+++
@@ -1,9 +0,0 @@
-local function keymaps()
-	vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-	vim.keymap.set('i', '<C-.>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-	vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-	vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-
-end
-
-return keymaps
