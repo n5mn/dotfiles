ae550b66eb60aab774755bd3d67a7039e97d6420
n5mn <n5mn@users.noreply.github.com>
2026-03-16 09:26:42 -0300

    refactor(nvim): simplify keymaps using local map alias

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -7,45 +7,44 @@ end
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex, opts)
+local map = vim.keymap.set
 
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", opts)
-
-vim.keymap.set({ "n", "x" }, "<leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to system clipboard" })
-
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", opts)
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", opts)
+map("n", "<leader>cd", vim.cmd.Ex, opts)
+map({ "n", "v" }, "<Space>", "<Nop>", opts)
+map({ "n", "x" }, "<leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to system clipboard" })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", opts)
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", opts)
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "]d", function()
+map("n", "]d", function()
 	vim.diagnostic.goto_next()
 end, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-vim.keymap.set("n", "<leader>l", vim.diagnostic.setqflist, { desc = "Open diagnostics quickfix list" })
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+map("n", "<leader>l", vim.diagnostic.setqflist, { desc = "Open diagnostics quickfix list" })
 
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+map("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+map("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
 
-vim.keymap.set("n", "<leader>/", telescope_live_grep_open_files, { desc = "[/] Search in Open Files" })
-vim.keymap.set("n", "<leader>ss", require("telescope.builtin").builtin, { desc = "[S]earch [S]elect Telescope" })
-vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-vim.keymap.set("n", "<leader>f", require("telescope.builtin").find_files, { desc = "[F]ind Files" })
-vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sG", ":LiveGrepGitRoot<cr>", { desc = "[S]earch by [G]rep on Git Root" })
-vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make file executable" })
+map("n", "<leader>/", telescope_live_grep_open_files, { desc = "[/] Search in Open Files" })
+map("n", "<leader>ss", require("telescope.builtin").builtin, { desc = "[S]earch [S]elect Telescope" })
+map("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
+map("n", "<leader>f", require("telescope.builtin").find_files, { desc = "[F]ind Files" })
+map("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+map("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+map("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+map("n", "<leader>sG", ":LiveGrepGitRoot<cr>", { desc = "[S]earch by [G]rep on Git Root" })
+map("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+map("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make file executable" })
 
 -- better indenting
-vim.keymap.set("v", "<", "<gv", opts)
-vim.keymap.set("v", ">", ">gv", opts)
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
-vim.keymap.set("n", "<C-c>", ":nohlsearch<CR>", { noremap = true, silent = true, desc = "Clear search highlight" })
+map("n", "<C-c>", ":nohlsearch<CR>", { noremap = true, silent = true, desc = "Clear search highlight" })
 
-vim.keymap.set("n",
+map("n",
 	"<leader>S",
 	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
 	{ desc = "Replace word cursor is on globally" }
@@ -61,7 +60,7 @@ local runners = {
 	javascript = "node %",
 }
 
-vim.keymap.set("n", "<leader>R", function ()
+map("n", "<leader>R", function ()
 	local filetype = vim.bo.filetype
 	local cmd = runners[filetype]
 	if cmd then
