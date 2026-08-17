ad9b6c49d23848d82c842af32c67ee6eeea1ae0d
n5mn <n5mn@users.noreply.github.com>
2026-08-17 02:54:46 -0400

    refactor(waybar): remove in favor of quickshell

--- .config/hypr/lua/autostart-example.lua
+++ .config/hypr/lua/autostart-example.lua
@@ -5,7 +5,6 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("~/dotfiles/scripts/update_wallpaper.sh init")
 	hl.exec_cmd("~/dotfiles/scripts/gtk.sh")
 	hl.exec_cmd("hyprctl setcursor Bibata-Original-Classic 24")
-	hl.exec_cmd("~/dotfiles/.config/waybar/launch.sh")
 	hl.exec_cmd("wl-paste --watch cliphist store")
 
 	hl.exec_cmd("alacritty", {workspace = "1 silent"})
